375369c82f56abd521c0a8f78e5ca6bfc6717af2
Theophilus Samuel <theosamuel2005@gmail.com>
2026-03-24 17:36:30 +0700

    refactor: restructure index.js for modular routing

--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
 const express = require("express");
 const app = express();
+
 app.use(express.json());
 
-let tasks = [];
+// routes
+const taskRoutes = require("./routes/taskRoutes");
+app.use("/tasks", taskRoutes);
 
-// CREATE
-app.post("/tasks", (req, res) => {
-    const task = {
-        id: Date.now(),
-        title: req.body.title,
-        isCompleted: false
-    };
-    tasks.push(task);
-    res.json({ status: "success", data: task });
-});
-
-// READ
-app.get("/tasks", (req, res) => {
-    res.json({ status: "success", data: tasks });
-});
-
-// UPDATE
-app.put("/tasks/:id", (req, res) => {
-    const task = tasks.find(t => t.id == req.params.id);
-    if (!task) return res.status(404).json({ status: "error" });
-
-    task.isCompleted = req.body.isCompleted ?? task.isCompleted;
-    res.json({ status: "success", data: task });
-});
-
-// DELETE
-app.delete("/tasks/:id", (req, res) => {
-    tasks = tasks.filter(t => t.id != req.params.id);
-    res.json({ status: "succcess" });
-});
-
-app.listen(3000, () => console.log("Server Running on Port 3000"));
+module.exports = app;
